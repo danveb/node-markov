46adec85dc57f272aa7ae8a93738ab09fa4b48e6
Daniel Bae <danibae91@icloud.com>
2021-09-02 22:04:57 -0700

    add code

--- makeText.js
+++ makeText.js
@@ -1,2 +1,40 @@
 /** Command-line tool to generate Markov text. */
 
+const fs = require('fs') 
+const process = require('process') 
+const axios = require('axios') 
+const { MarkovMachine } = require('./markov')
+
+// generateText(text) 
+function generateText(text) {
+    let mm = new MarkovMachine(text) 
+    console.log(mm.makeText()) 
+}
+
+// makeText 
+function makeText(path) {
+    fs.readFile(path, 'utf-8', (err, data) => {
+        if(err) {
+            console.log(`Error reading file ${path}, ${err}`) 
+            process.exit(1) 
+        }
+        generateText(data) 
+    })
+}
+
+// makeURLText 
+async function makeURLText(url) {
+    try {
+        let response = await axios.get(url) 
+        return generateText(response.data) 
+    } catch(err) {
+        console.log(`Error with ${url}: ${err}`)
+    }
+}
+
+let [method, path] = process.argv.slice(2) 
+if(method === 'file') {
+    makeText(path) 
+} else if(method === 'url') {
+    makeURLText(path) 
+} 
--- markov.js
+++ markov.js
@@ -16,14 +16,60 @@ class MarkovMachine {
    *  for text of "the cat in the hat", chains will be
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
-  makeChains() {
+   makeChains() {
     // TODO
+    // start with empty object
+    const chain = {} 
+    const words = this.words
+
+    // Iterate this.words pushing next to chain[word] 
+    for(let i = 0; i < words.length; i++) {
+      let word = this.words[i]
+      let next = this.words[i + 1]
+  
+      // For new word, set word as key in chain, set val as next, or if no next set val to null 
+      if(!chain[word]) {
+        chain[word] = !next ? [null] : [next] 
+      } else {
+        // if chain has word as key and push next to array; if no next then push null 
+        chain[word].push(!next ? null : next) 
+      }
+    }
+    // set object property
+    this.chain = chain; 
   }
 
+  // Function to return random element from array 
+  randomArrayElement(array) {
+    return array[Math.floor(Math.random() * array.length)] 
+  }
 
   /** return random text from chains */
 
   makeText(numWords = 100) {
     // TODO
+    // Random first word 
+    let word = this.randomArrayElement(Object.keys(this.chain))
+    let text = word
+
+    // Counter for loop 
+    let counter = 1; 
+    // Loop to concat new words to "text" string 
+    while(counter <= numWords) {
+      // set "word" to random element in this.chain item array 
+      word = this.randomArrayElement(this.chain[word])
+      // break at null 
+      if(word === null) {
+        break; 
+      }
+
+      // concat new words to text 
+      text += `${word}`
+      counter++; 
+    }
+    return text; 
   }
 }
+
+// module.exports 
+module.exports = { MarkovMachine }
